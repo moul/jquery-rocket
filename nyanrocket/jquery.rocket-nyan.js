e33f41f672f6ac5c9518e429276c310f4bedd43a
Manfred Touron <m@42.am>
2012-01-06 18:02:06 +0100

    .

--- nyanrocket/jquery.rocket-nyan.js
+++ nyanrocket/jquery.rocket-nyan.js
@@ -7,11 +7,9 @@ var VroumVroum  = '38,38,40,40,37,39,37,39,66,65';
 $(document)
     .keydown(
         function(e) {
-            keys.push( e.keyCode );
-            if ( keys.toString().indexOf( VroumVroum ) >= 0 ){
-
-                $('body').append('<div id="KCode" style="position:absolute; z-index:1337;left:0px; top:0px; bottom:-30px; right:0px; display:none; background:url(/im/nrbg.gif) ; padding-left:50%; padding-top:20%"><img src="/im/nyanrocket.gif" style="margin-top:-160px; margin-left:-315px;"/></div><div id="loveboat" style="position:absolute; height:1px;width:1px; top:-100px;"></div>');
-                /*loadScript("http://swfobject.googlecode.com/svn/trunk/swfobject/swfobject.js", function(){});*/
+            keys.push(e.keyCode);
+            if (keys.toString().indexOf(VroumVroum) >= 0 ){
+                $('body').append('<div id="KCode" style="position:absolute; z-index:1337;left:0px; top:0px; bottom:-30px; right:0px; display:none; background:url(nrbg.gif) ; padding-left:50%; padding-top:20%"><img src="nyanrocket.gif" style="margin-top:-160px; margin-left:-315px;"/></div><div id="loveboat" style="position:absolute; height:1px;width:1px; top:-100px;"></div>');
                 var atts = { id: "video" };
                 var params = {
                     allowScriptAccess: "always",
@@ -19,9 +17,7 @@ $(document)
                 };
                 swfobject.embedSWF("http://www.youtube.com/apiplayer?video_id=icAwcByaNtY&version=3&autoplay=1&enablejsapi=1&playerapiid=ytplayer","loveboat", "1", "1", "8", null, null, params, atts);
                 $('#KCode').fadeIn(2000);
-
                 keys = [];
             }
         }
     );
-)
