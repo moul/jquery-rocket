2a0bdbdfc9fa3dffd6e64c5a568104fe949e14cc
Manfred Touron <m@42.am>
2012-01-07 23:06:32 +0100

    ,

--- rocket-3d/web/jquery.rocket-3d.js
+++ rocket-3d/web/jquery.rocket-3d.js
@@ -34,15 +34,22 @@
 				  camera = new THREE.PerspectiveCamera(40, _width / _height, 1, 10000);
 				  camera.position.z = 1800;
 				  scene = new THREE.Scene();
+
+                                  scene.fog = new THREE.FogExp2( 0xffffff, 0.0003 );
+                                  //scene.fog.color.setHSV( 0.1, 0.5, 1 );
+                                  scene.fog.color.setHSV( 0.1, 0.10, 1 );
+
 				  light = new THREE.DirectionalLight(0xffffff);
 				  light.position.set(0, 0, 1).normalize();
 				  scene.add(light);
 				  var loader = new THREE.JSONLoader();
+
+
 				  loader.load("obj/rocket3d.js", function (geometry) {
 				                  geometry.materials[0].shading = THREE.FlatShading;
                                                   mesh = new THREE.Object3D();
 				                  //mesh.position.x = _width / 2;
-                                                  mesh.position.x = _width / 2 - 100;
+                                                  mesh.position.x = _width / 2;
 				                  mesh.position.y = -_height / 2 + 50;
 				                  mesh.position.z = 0;
                                                   mesh.rotation.x = 0;
@@ -55,6 +62,7 @@
                                                   //var part2 = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({ color: 0xff0000, opacity: 0.9, shading: THREE.FlatShading, wireframe: true, wireframeLinewidth: 2, transparent: true }));
 				                  //mesh.add(part2);
                                               });
+
 				  renderer = new THREE.WebGLRenderer({antialias: true});
 				  renderer.setSize(_width, _height);
                                   container.append(renderer.domElement);
@@ -72,6 +80,7 @@
 				                                mouseY = (event.clientY - windowHalfY);
                                                             }, false);*/
                                   //container.parent().css('overflow', 'hidden');
+                                  var step = 0;
                                   $("html:not(:animated),body:not(:animated)").animate({scrollTop: _height - $(window).height()}, 500, animate);
 			          function animate() {
 				      requestAnimationFrame(animate);
@@ -80,18 +89,50 @@
 				      camera.position.y += (-mouseY - camera.position.y ) * 1;
 				      camera.lookAt(scene.position);
 
-				      if (mesh) {
+                                      if (step == 0) {
 					  //mesh.rotation.x += 0.00;
-					  mesh.rotation.y += 0.01;
+					  mesh.rotation.y += 0.02;
+                                          mesh.position.x -= 7;
 					  //mesh.rotation.z += 0.01;
                                           //mesh.scale.x += 0.1;
                                           //mesh.scale.y += 0.1;
                                           //mesh.scale.z += 0.1;
-				      }
-				      if (mesh2) {
-					  mesh2.rotation.x += 0.01;
-					  mesh2.rotation.y += 0.01;
-				      }
+                                          if (mesh.position.x <= -_width / 2 + 200) {
+                                              step = 1;
+                                          }
+				      } else if (step == 1) {
+                                          mesh.rotation.y += 0.01;
+                                          if (mesh.rotation.y >= 4.5) {
+                                              step = 2;
+                                          }
+				      } else if (step == 2) {
+                                          mesh.rotation.y += 0.01;
+                                          mesh.rotation.x -= 0.01;
+                                          if (mesh.rotation.x <= -0.3) {
+                                              step = 3;
+                                          }
+                                      } else if (step == 3) {
+                                          mesh.rotation.y += 0.01;
+                                          mesh.rotation.z -= 0.01;
+                                          if (mesh.rotation.z <= -0.4) {
+                                              step = 4;
+                                          }
+                                      } else if (step == 4) {
+                                          mesh.rotation.y += 0.01;
+                                          //mesh.position.x += 0.01;
+                                          //alert(mesh.rotation.y);
+                                          //mesh.p
+                                          if (mesh.rotation.y > 5.9) {
+                                              step = 5;
+                                          }
+                                      } else if (step == 5) {
+                                          mesh.rotation.y -= 0.01;
+                                          if (mesh.rotation.y < 5.3) {
+                                              step = 6;
+                                          }
+                                      } else if (step == 6) {
+                                          step = 7;
+                                      }
 
 				      renderer.render(scene, camera);
 
